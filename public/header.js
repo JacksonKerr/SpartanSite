256b04bcdc5061dc12e545e00806a75e7024cd89
Jackson Kerr <jacksonkerr2@gmail.com>
2025-02-01 14:24:29 +1300

    Reformat to /blog instead of the ugly pages/blog/blog

--- public/header.js
+++ public/header.js
@@ -2,6 +2,6 @@ document.write(`
     <div class="secondary centerText">
         <h1 style="padding: 5px; margin: 0;">Welcome to my site</h1>
         <a href="\\">Home</a>
-        <a href="\\pages\\blog\\blog.html">Blog</a>
+        <a href="blog">Blog</a>
     </div>
 `);
