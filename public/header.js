cd36998046035bda8fab80dacbb7199d2d9c0969
Jackson Kerr <jacksonkerr2@gmail.com>
2025-02-03 22:15:46 +1300

    Screw it. Just use a single file.

--- public/header.js
+++
@@ -1,7 +0,0 @@
-document.write(`
-    <div class="secondary centerText">
-        <h1 style="padding: 5px; margin: 0;">Welcome to my site</h1>
-        <a href="\\">Home</a>
-        <a href="blog">Blog</a>
-    </div>
-`);
